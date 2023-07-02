d3817f547be492290ce12130b63ec27d0e6a90cc
Yuri <zvozdjak.y@gmail.com>
2023-07-02 20:12:01 +0300

    animations

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,35 +1,57 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
 
-    private float _playerForce = 5f;
+    [FormerlySerializedAs("_playerForce")] [SerializeField] private float playerForce = 5f;
+    private float _horizontalAxisValue;
+    private static readonly int Running = Animator.StringToHash("running");
 
     float VerticalForce()
     {
-        return _playerForce * 1.5f;
+        return playerForce * 1.5f;
     }
     
     float HorizontalForce()
     {
-        return _playerForce;
+        return playerForce;
     }
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        float horizontalAxisValue = Input.GetAxisRaw("Horizontal");
+        _horizontalAxisValue = Input.GetAxisRaw("Horizontal");
         
-        _rigidbody2D.velocity = new Vector3(HorizontalForce() * horizontalAxisValue, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector3(HorizontalForce() * _horizontalAxisValue, _rigidbody2D.velocity.y);
         
-        if (Input.GetButtonDown("Jump") && _rigidbody2D.velocity.y == 0)
+        if (Input.GetButtonDown("Jump") && _rigidbody2D.position.y == 0)
         {
             _rigidbody2D.velocity = new Vector3(_rigidbody2D.velocity.x, VerticalForce());
+        }
+        
+        UpdateAnimations();
+    }
+
+    private void UpdateAnimations()
+    {
+        if (_horizontalAxisValue == 0f)
+        {
+            _animator.SetBool(Running, false);
+        }
+        else
+        {
+            _animator.SetBool(Running, true);
+            _spriteRenderer.flipX = _horizontalAxisValue < 0f;
         }
     }
 }
