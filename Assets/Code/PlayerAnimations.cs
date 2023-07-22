98e2dcbf38833a4b5a635c9f25e3f6eb679942a6
Yuri <zvozdjak.y@gmail.com>
2023-07-22 16:38:54 +0300

    animations

---
+++ Assets/Code/PlayerAnimations.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    
+    private static readonly int AnimatorStateField = Animator.StringToHash("state");
+    
+    private enum MovementState { Idle, Running, Jumping, Falling}
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateAnimations(_rigidbody2D.velocity);
+    }
+
+    private void UpdateAnimations(Vector2 velocity)
+    {
+        MovementState state;
+        
+        // animations when on air
+        if (Math.Abs(velocity.y) > .1f)
+        {
+            state = velocity.y > 0 ? MovementState.Jumping : MovementState.Falling;
+        }
+        // animations when on ground
+        else
+        {
+            if (Math.Abs(velocity.x) < .1f)
+            {
+                state = MovementState.Idle;
+            }
+            else
+            {
+                state = MovementState.Running;
+                _spriteRenderer.flipX = velocity.x < 0f;
+            }
+        }
+        _animator.SetInteger(AnimatorStateField, (int)state);
+    }
+}
--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,11 +1,8 @@
-using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
-    private Animator _animator;
-    private SpriteRenderer _spriteRenderer;
     private BoxCollider2D _collider;
 
     [SerializeField] private float verticalForce = 7f;
@@ -15,13 +12,10 @@ public class PlayerMovement : MonoBehaviour
     private float _distToGround;
 
     private float _horizontalAxisValue;
-    private static readonly int Running = Animator.StringToHash("running");
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
         _collider = GetComponent<BoxCollider2D>();
         _distToGround = _collider.bounds.extents.y;
     }
@@ -32,8 +26,6 @@ public class PlayerMovement : MonoBehaviour
         {
             UpdatePlayerVelocity();
         }
-
-        UpdateAnimations();
     }
 
     private void UpdatePlayerVelocity()
@@ -45,27 +37,6 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             _rigidbody2D.velocity = new Vector3(_rigidbody2D.velocity.x, verticalForce);
-        }
-    }
-
-    private void UpdateAnimations()
-    {
-        // animations when on air
-        if (Math.Abs(_rigidbody2D.velocity.y) > .1f)
-        {
-        }
-        // animations when on ground
-        else
-        {
-            if (_horizontalAxisValue == 0f)
-            {
-                _animator.SetBool(Running, false);
-            }
-            else
-            {
-                _animator.SetBool(Running, true);
-                _spriteRenderer.flipX = _horizontalAxisValue < 0f;
-            }
         }
     }
 
