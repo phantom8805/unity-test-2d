82c12087cc707ecd4e7834e1ac1a27cc79178e13
Yuri <zvozdjak.y@gmail.com>
2023-07-23 14:40:38 +0300

    Player Life

---
+++ Assets/Code/PlayerLife.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
+    private static readonly int Dead = Animator.StringToHash("dead");
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            DieAnimationAndDisable();
+        }
+    }
+
+    private void DieAnimationAndDisable()
+    {
+        _animator.SetTrigger(Dead);
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.simulated = false;
+    }
+    
+    private void OnDieAnimationEnd()
+    {
+        SceneManager.LoadScene(gameObject.scene.name);
+    }
+}
